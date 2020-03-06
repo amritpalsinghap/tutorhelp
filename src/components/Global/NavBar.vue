f0ed6789b5136ea7dc5cf283c17609c1b4bafd70
unknown <amritpal500.ap@gmail.com>
2020-03-05 23:39:22 -0500

    Intiate NavBar.vue

---
+++ src/components/Global/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="info">
+      <b-navbar-brand to="/">
+        <img src="../../assets/flyerPic.png"  width= "30px" height= "40px" class="d-inline-block align-top" alt="TutorHelp">TutorHelp
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/submtRequest">SubmitProblem</b-nav-item>
+          <b-nav-item href="#">Disabled</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavBar",
+ 
+};
+</script>
+<style>
+</style>
