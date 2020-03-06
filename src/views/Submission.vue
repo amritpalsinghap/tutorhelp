524234d4fc20f957e5a110661bc2d040e690c577
unknown <amritpal500.ap@gmail.com>
2020-03-05 23:42:03 -0500

    intiate Submission.vue

---
+++ src/views/Submission.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-container>
+      <b-row id="popupsection">
+        <b-col cols="6">
+          <div id="row-2">
+            <img src="../assets/flyerPic.png" width="300px" height="700px" />
+          </div>
+        </b-col>
+        <b-col cols="6">
+          <cstmForm></cstmForm>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+import cstmForm from "@/components/form";
+export default {
+  name: "Submission",
+  components: {
+    cstmForm
+  }
+};
+</script>
+<style >
+#popupsection {
+  background-color: aqua;
+}
+</style>
