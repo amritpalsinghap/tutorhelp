b98ad5776ad25a3ce2ff447697fae49d8e32136f
unknown <amritpal500.ap@gmail.com>
2020-05-13 14:23:50 -0400

    remove css and move to the FrontPage.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <div class="blur-bg"></div>
     <cstm-navbar></cstm-navbar>
     <router-view />
   </div>
@@ -12,21 +11,6 @@ export default {
 </script>
 
 <style>
-.blur-bg {
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  -webkit-filter: blur(8px);
-  filter: blur(8px);
-  background: url("assets/background.jpg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  z-index: -1;
-}
-
 body,
 html {
   height: 100%;
