18e2adb5d08778522f7bcf5ff334c6bac1d9ccc7
unknown <amritpal500.ap@gmail.com>
2020-05-13 14:22:23 -0400

    Delete HelloWorld.vue, Added FrontPage.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <FrontPage msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import FrontPage from "@/components/FrontPage.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    FrontPage
   }
 };
 </script>
