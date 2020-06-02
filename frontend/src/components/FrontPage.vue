2bb13d5c744e20b77412dc49465f755743b1e869
amritpalsinghap <amritpal500.ap@gmail.com>
2020-06-02 18:08:49 -0400

    insert ImageSlider component

--- frontend/src/components/FrontPage.vue
+++ frontend/src/components/FrontPage.vue
@@ -1,27 +1,15 @@
 <template>
-  <div class="blur-bg">
-    
+  <div >
+    <cstm-imgslider></cstm-imgslider>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home"
+  
 };
 </script>
 <style scoped lang="scss">
-.blur-bg {
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  -webkit-filter: blur(8px);
-  filter: blur(8px);
-  background: url("../assets/background.jpg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  z-index: -1;
-}
+
 </style>
