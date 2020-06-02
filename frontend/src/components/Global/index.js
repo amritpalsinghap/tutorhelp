bef2dadc68cd28e968eed0225702d5e20463db6a
amritpalsinghap <amritpal500.ap@gmail.com>
2020-06-02 18:07:41 -0400

    register ImageSlider and goBack components

--- frontend/src/components/Global/index.js
+++ frontend/src/components/Global/index.js
@@ -1,3 +1,5 @@
 import vue from "vue";
 
 vue.component("cstm-navbar", require("./NavBar.vue").default);
+vue.component("cstm-imgslider", require("./ImageSlider.vue").default);
+vue.component("cstm-goBackbtn", require("./goBack.vue").default);
