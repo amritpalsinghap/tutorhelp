d115dbe9b94ff4dc52b3055f5fe91e5a8281293f
amritpalsinghap <amritpal500.ap@gmail.com>
2020-06-07 21:38:33 -0400

    initiate component
    vertical slide for the mobile

---
+++ frontend/src/components/ImageSliderVertical.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pagesliderVertical">
+    <div class="page1"><h2>Page1</h2></div>
+    <div class="page2"><h2>Page2</h2></div>
+    <div class="page3"><h2>Page3</h2></div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
