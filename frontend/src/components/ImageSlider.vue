5c9a1549fe6f6feba9efeb1182cb6311b69e53c1
amritpalsinghap <amritpal500.ap@gmail.com>
2020-06-07 21:39:00 -0400

    change location
    from Global to components

---
+++ frontend/src/components/ImageSlider.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="imageSliderSection">
+    <b-carousel id="carousel-1" :interval="4000" controls indicators>
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        img-src="../assets/backgroundImageLanguageList.svg"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="../assets/backgroundImageTeaching.svg">
+        <template>
+          <div id="teachingSection">
+            <router-link to="/submitRequest">
+              <button class="btn SubmitProblemButton">
+                <img
+                  src="../assets/submitProblemButton.svg"
+                  alt="SubmitProblemButton"
+                />
+              </button>
+            </router-link>
+          </div>
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide img-src="../assets/backgroundImageAboutUs.svg"
+        ><p></p
+      ></b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "imageSlide",
+};
+</script>
+<style lang="scss">
+@import "src/custom.scss";
+
+.carousel-caption {
+  top: 10rem;
+  bottom: auto;
+}
+.SubmitProblemButton {
+  width: 180px;
+  height: 90px;
+}
+</style>
