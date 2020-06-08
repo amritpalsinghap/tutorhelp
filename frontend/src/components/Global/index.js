e83a300a3714896a4b4dbe8d7aed37bda92391f0
amritpalsinghap <amritpal500.ap@gmail.com>
2020-06-07 21:40:49 -0400

    refractor content
    remove unnecessary components registeration

--- frontend/src/components/Global/index.js
+++ frontend/src/components/Global/index.js
@@ -1,5 +1,5 @@
 import vue from "vue";
 
 vue.component("cstm-navbar", require("./NavBar.vue").default);
-vue.component("cstm-imgslider", require("./ImageSlider.vue").default);
-vue.component("cstm-goBackbtn", require("./goBack.vue").default);
+vue.component("cstm-gobackbtn", require("./GoBack.vue").default);
+vue.component("cstm-mouserscroller", require("./MouseScroller.vue").default);
