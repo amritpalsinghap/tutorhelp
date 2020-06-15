f140ebbd0717af535cc1e82583ebce26c4faab0e
amritpalsinghap <amritpal500.ap@gmail.com>
2020-06-15 19:40:27 -0400

    refractor

--- frontend/src/components/Global/NavBar.vue
+++ frontend/src/components/Global/NavBar.vue
@@ -12,9 +12,7 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item to="/submitRequest" class="btn btn-2"
-            >SubmitProblem</b-nav-item
-          >
+          <b-nav-item to="/submitRequest" class="btn btn-2">SubmitProblem</b-nav-item>
           <b-nav-item to="/about" class="btn btn-2">About us</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
@@ -33,12 +31,13 @@ export default {
 <style lang="scss" scoped>
 @import "src/custom.scss";
 
-@include media-breakpoint-down(md) {
+@include media-breakpoint-down(sm) {
   .navArea {
     overflow: hidden;
     position: fixed;
     width: 100%;
-  
+    padding-left:0;
+    padding-right:0;
   }
 }
 @include media-breakpoint-up(lg) {
