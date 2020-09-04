78f7b8227a3a508ffcddc2bb2f32c9f0f71c75c4
amritpalsinghap <amritpal500.ap@gmail.com>
2020-09-04 18:50:25 -0400

    Update App.vue
    Remove Custom Model Component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div id="app">
     <cstm-navbar></cstm-navbar>
-    <cstmModal></cstmModal>
     <router-view />
   </div>
 </template>
 <script>
-import cstmModal from "@/components/ModalTest";
 export default {
   name: "App",
-  components: {
-    cstmModal,
-  },
 };
 </script>
 
@@ -21,6 +16,4 @@ html {
   background-color: #00d0d0;
   font-family: "Franklin Gothic Medium", "Arial Narrow", "Arial", "sans-serif";
 }
-
-
 </style>
