40a82d3adc4eb89ba6153172da10b2e8c3691d82
amritpalsinghap <amritpal500.ap@gmail.com>
2020-09-14 22:10:48 -0400

    Update SlideIndex.js
    create
    1, state
    2, getters
    3, actions
    4, mutations
    5, export

--- frontend/src/store/modules/SlideIndex.js
+++ frontend/src/store/modules/SlideIndex.js
@@ -1,14 +1,47 @@
-export const store = {
-    debug: true,
-    state: {
-      SlideIndex: 0,
-    },
-    setSlideIndex(newValue) {
-      if (this.debug) console.log("setSlideIndex triggered with", newValue);
-      this.state.SlideIndex = newValue;
-    },
-    clearSlideIndex() {
-      if (this.debug) console.log("clearSlideIndex triggered");
-      this.state.SlideIndex = "";
-    },
-  };
+//state
+const state = {
+  SlideIndex: 0,
+};
+//getters
+const getters = {
+  slideIndexValue: (state) => {
+    return state.SlideIndex;
+  },
+};
+
+//actions
+const actions = {
+  SlideIndex(commit, slideIndexValue, callback) {
+    if (slideIndexValue >= 0 && slideIndexValue <= 2) {
+      commit("setSlideIndex", slideIndexValue);
+      callback("slideIndex Set to : ", slideIndexValue);
+    } else {
+      callback("ERROR while setting the slideIndex");
+    }
+  },
+};
+
+//mutations
+const mutations = {
+  setSlideIndex(state, slideIndexValue) {
+    state.SlideIndex = slideIndexValue;
+  },
+};
+
+//export
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
+
+// setSlideIndex(newValue) {
+//   if (this.debug) console.log("setSlideIndex triggered with", newValue);
+//   this.state.SlideIndex = newValue;
+// }
+// clearSlideIndex() {
+//   if (this.debug) console.log("clearSlideIndex triggered");
+//   this.state.SlideIndex = "";
+// }
